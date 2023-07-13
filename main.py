a2839861be34e82a68921725e49834534da19031
ReimuJF <slavalang@gmail.com>
2023-07-13 17:42:43 +0500

    1307 update
    added two classes
    - class for gui
    - class for login functions

--- main.py
+++ main.py
@@ -1,62 +1,67 @@
-from usermenu import *
+from userlogin import Login
 import tkinter as tk
 import tkinter.messagebox
 
 
-
-def login_button():
-    check = user_login(entry_login.get(), entry_password.get())
-    if check:
-        tkinter.messagebox.showinfo("Success", f"Hello {entry_login.get()}")
-        clear_entry()
-    else:
-        tkinter.messagebox.showinfo("Error", "Wrong login or password")
-
-
-def sign_button():
-    check, check_pass = create_user(entry_login.get(), entry_password.get())
-    if check:
-        tkinter.messagebox.showinfo("Success", f"User {entry_login.get()} created")
-        tkinter.messagebox.showinfo("Success", f"your pass: {check_pass}" if
-        check_pass != entry_password.get() else "Remember your password")
-        clear_entry()
-    else:
-        tkinter.messagebox.showinfo("Error", "This name already taken")
-
-
-def get_button():
-    users_list = get_list()
-    tkinter.messagebox.showinfo("Success", f"{users_list}")
+class LoginWindow(tk.Tk):
+    def __init__(self):
+        self.Login = Login()
+        super().__init__()
+        self.title('Simple Login Page')
+        self.resizable(width=False, height=False)
+        self.geometry(f'310x110+{(self.winfo_screenwidth()//2)-300}+{(self.winfo_screenheight()//2)-300}')
+        self.entry_login = tk.Entry(width=25)
+        self.entry_password = tk.Entry(width=25, show="*")
+        self.entry_login.grid(row=0, column=0, padx=10)
+        self.entry_password.grid(row=1, column=0, padx=10)
+        self.button_login = tk.Button(text='LOGIN', width=15, command=self.login_button)
+        self.button_login.grid(row=0, column=1, padx=10, pady=5)
+        self.button_sign = tk.Button(text='SIGN UP', width=15, command=self.sign_button)
+        self.button_sign.grid(row=1, column=1, padx=10)
+        self.button_get = tk.Button(text='GET USER LIST', width=15, command=self.get_button)
+        self.button_get.grid(row=2, column=0, pady=10, sticky='ws', padx=5)
+        self.button_delete = tk.Button(text='DELETE USER', width=15, command=self.delete_button)
+        self.button_delete.grid(row=2, column=1, pady=10, sticky='es', padx=10)
 
 
-def delete_button():
-    check = delete_user(entry_login.get())
-    if check:
-        tkinter.messagebox.showinfo("Success", f"User was deleted")
-        clear_entry()
-    else:
-        tkinter.messagebox.showinfo("Error", "User not found")
+    def login_button(self):
+        check = self.Login.user_login(self.entry_login.get(), self.entry_password.get())
+        if check:
+            tkinter.messagebox.showinfo("Success", f"Hello {self.entry_login.get()}")
+            self.clear_entry()
+        else:
+            tkinter.messagebox.showinfo("Error", "Wrong login or password")
 
 
-def clear_entry():
-    entry_login.delete(0, tk.END)
-    entry_password.delete(0, tk.END)
+    def sign_button(self):
+        check, check_pass = self.Login.create_user(self.entry_login.get(), self.entry_password.get())
+        if check:
+            tkinter.messagebox.showinfo("Success", f"User {self.entry_login.get()} created")
+            tkinter.messagebox.showinfo("Success", f"your password: {check_pass}" if
+            check_pass != self.entry_password.get() else "Remember your password")
+            self.clear_entry()
+        else:
+            tkinter.messagebox.showinfo("Error", "This name already taken")
+
+
+    def get_button(self):
+        users_list = self.Login.get_list()
+        tkinter.messagebox.showinfo("Success", f"{users_list}")
+
+
+    def delete_button(self):
+        check = self.Login.delete_user(self.entry_login.get())
+        if check:
+            tkinter.messagebox.showinfo("Success", "User was deleted")
+            self.clear_entry()
+        else:
+            tkinter.messagebox.showinfo("Error", "User not found")
+
+
+    def clear_entry(self):
+        self.entry_login.delete(0, tk.END)
+        self.entry_password.delete(0, tk.END)
 
 if __name__ == "__main__":
-    window = tk.Tk()
-    window.title('Simple Login Page')
-    window.resizable(width=False, height=False)
-
-    entry_login = tk.Entry(width=25)
-    entry_password = tk.Entry(width=25, show="*")
-    entry_login.grid(row=0, column=0, padx=10)
-    entry_password.grid(row=1, column=0, padx=10)
-    button_login = tk.Button(text='LOGIN', width=15, command=login_button)
-    button_login.grid(row=0, column=1, padx=10, pady=5)
-    button_sign = tk.Button(text='SIGN UP', width=15, command=sign_button)
-    button_sign.grid(row=1, column=1, padx=10)
-    button_get = tk.Button(text='GET USER LIST', width=15, command=get_button)
-    button_get.grid(row=2, column=0, pady=10, sticky='ws', padx=5)
-    button_delete = tk.Button(text='DELETE USER', width=15, command=delete_button)
-    button_delete.grid(row=2, column=1, pady=10, sticky='es', padx=10)
+    window = LoginWindow()
     window.mainloop()
---
+++ userlogin.py
@@ -0,0 +1,85 @@
+from password_generator import PasswordGenerator
+import argon2
+from cryptography.fernet import Fernet
+import json
+import os
+import secret #secret key for Fernet you need to create your own
+
+
+class Login:
+    def __init__(self):
+        self.ph = argon2.PasswordHasher()
+        self.fernet = Fernet(secret.s_key)
+        self.dbfile = './users.dic'
+        if not os.path.isfile(self.dbfile):
+            with open(self.dbfile, 'w') as f:
+                f.write('{}')
+            self.encrypt_file()
+    def hash_pass(self,password):
+        hashed_pass = self.ph.hash(password)
+        return hashed_pass
+
+    def open_file(self):
+        with open('./users.dic') as encr_db:
+            encrypted_dict = encr_db.read()
+        decrypted_dict = self.fernet.decrypt(encrypted_dict)
+        return json.loads(decrypted_dict)
+
+    def write_file(self, js_dict):
+        dict_json = json.dumps(js_dict)
+        with open(self.dbfile, 'w') as db:
+            db.write(dict_json)
+        self.encrypt_file()
+
+    def encrypt_file(self):
+        with open(self.dbfile, 'rb') as file:
+            original = file.read()
+
+        dict_encrypted = self.fernet.encrypt(original)
+
+        with open(self.dbfile, 'wb') as enc_file:
+            enc_file.write(dict_encrypted)
+
+    @staticmethod
+    def generate_password():
+        rng_password = PasswordGenerator()
+        rng_password.minlen = 4
+        rng_password.maxlen = 8
+        rng_password.minnumbers = 1
+        rng_password.minschars = 1
+        rng_password.minuchars = 1
+        return rng_password.generate()
+
+
+    def create_user(self, login, password):
+        js_dict = self.open_file()
+        if login in js_dict:
+            return False, None
+        passwordo = password
+        if not password:
+            passwordo = self.generate_password()
+        js_dict[login] = self.hash_pass(passwordo)
+        self.write_file(js_dict)
+        return True, passwordo
+
+
+    def user_login(self, login, password):
+        js_dict = self.open_file()
+        ph = argon2.PasswordHasher()
+        try:
+            return ph.verify(js_dict[login], password)
+        except (argon2.exceptions.VerifyMismatchError, KeyError):
+            return False
+
+    def delete_user(self, user_name):
+        js_dict = self.open_file()
+        if user_name not in js_dict:
+            return False
+        del js_dict[user_name]
+        self.write_file(js_dict)
+        return True
+
+
+    def get_list(self):
+        js_dict = self.open_file()
+        return '\n'.join(js_dict.keys())  # users_list
